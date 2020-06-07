5941a76da8d1dddfe426ffc0c3ac45451f9c860b
BernardoHaab <bernardohaab@gmail.com>
2020-06-07 15:57:53 -0300

    First version of project

---
+++ server/src/database/migrations/02_create_point_items.ts
@@ -0,0 +1,21 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+    return knex.schema.createTable('point_items', tableBuilder => {
+        tableBuilder.increments('id').primary();
+
+        tableBuilder.integer('point_id')
+            .notNullable()
+            .references('id')
+            .inTable('points');
+
+        tableBuilder.integer('item_id')
+            .notNullable()
+            .references('id')
+            .inTable('items');
+    })
+}
+
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('point_tems')
+}
